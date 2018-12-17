2a00c386bd19723c864be35e41009061a3312abe
Antoine CLOP <antoine.clop@epita.fr>
2018-12-17 11:03:35 +0100

    [FEATURED] Creating featured game list screen

---
+++ LeagueAPIDemo/LeagueAPIDemo/Controllers/FeaturedGamesViewController.swift
@@ -0,0 +1,91 @@
+//
+//  FeaturedGamesViewController.swift
+//  LeagueAPIDemo
+//
+//  Created by Antoine Clop on 12/17/18.
+//  Copyright © 2018 Antoine Clop. All rights reserved.
+//
+
+import UIKit
+import LeagueAPI
+
+class FeaturedGamesViewController: UIViewController {
+
+    // MARK: - IBOulet
+    
+    @IBOutlet weak var featuredGamesTableView: UITableView!
+    
+    // MARK: - Variables
+    
+    var games: [GameInfo] = []
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.getFeaturedGames()
+    }
+    
+    // MARK: - Functions
+    
+    func getFeaturedGames() {
+        league.riotAPI.getFeaturedGames(on: preferedRegion) { (featuredGames, errorMsg) in
+            if let featuredGames = featuredGames {
+                self.games = featuredGames.games
+                self.featuredGamesTableView.reload()
+            }
+            else {
+                print("Request failed cause: \(errorMsg ?? "No error description")")
+            }
+        }
+    }
+    
+    func getChampionImage(championId: ChampionId, completion: @escaping (UIImage?) -> Void) {
+        league.getChampionDetails(by: championId) { (champion, errorMsg) in
+            if let champion = champion, let defaultSkin = champion.images?.loading {
+                defaultSkin.getImage() { (image, error) in
+                    completion(image)
+                }
+            }
+            else {
+                print("Request failed cause: \(errorMsg ?? "No error description")")
+            }
+        }
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
+
+extension FeaturedGamesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.games.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let gameAtIndex: GameInfo = self.games[indexPath.row]
+        return setupGameCell(for: gameAtIndex)
+    }
+    
+    func setupGameCell(for game: GameInfo) -> GameTableViewCell {
+        let newCell: GameTableViewCell = self.featuredGamesTableView.dequeueReusableCell(withIdentifier: "gameCell") as! GameTableViewCell
+        if let firstParticipant = game.participants.first {
+            let presentationChampId: ChampionId = firstParticipant.championId
+            self.getChampionImage(championId: presentationChampId) { image in
+                newCell.backgroundImage.setImage(image)
+            }
+        }
+        return newCell
+    }
+}
+
+extension FeaturedGamesViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let gameAtIndex: GameInfo = self.games[indexPath.row]
+        self.performSegue(withIdentifier: "showGame", sender: gameAtIndex)
+    }
+}
