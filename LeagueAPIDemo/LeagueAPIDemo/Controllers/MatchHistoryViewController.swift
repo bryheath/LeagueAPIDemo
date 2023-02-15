2554b8b3f99317625461d33f6099862b887d88c0
Bryan Heath <bryheath86@gmail.com>
2023-02-14 23:39:40 -0600

    Refactoring MatchHistoryViewController

--- LeagueAPIDemo/LeagueAPIDemo/Controllers/MatchHistoryViewController.swift
+++ LeagueAPIDemo/LeagueAPIDemo/Controllers/MatchHistoryViewController.swift
@@ -19,8 +19,8 @@ class MatchHistoryViewController: UIViewController {
     // MARK: - Variables
     
     private var summoner: Summoner?
-    private var matches: [MatchReference] = []
-    private var matchDetails: [GameId : Match] = [:]
+    private var matches: [LOLMatchId] = []
+    private var matchDetails: [LOLMatchId : Match] = [:]
     
     // MARK: - View Lifecycle
     override func viewDidLoad() {
@@ -33,15 +33,18 @@ class MatchHistoryViewController: UIViewController {
         league.lolAPI.getSummoner(byName: summonerName, on: preferredRegion) { (summoner, errorMsg) in
             if let summoner = summoner {
                 self.summoner = summoner
-                league.lolAPI.getMatchList(by: summoner.accountId, on: preferredRegion, endIndex: 20) { (matchList, errorMsg) in
-                    if let matchList = matchList {
-                        self.matches = matchList.matches
-                        self.matchHistoryTableView.reload()
-                    }
-                    else {
-                        print("Request failed cause: \(errorMsg ?? "No error description")")
-                    }
+                league.lolAPI.getMatchList(by: summoner.puuid, on: preferredRegion, count: 20) { (matchList, errorMsg) in
+                   
+//                    if let matchList = matchList {
+//                        matchList.
+////                        self.matches = matchList
+//                        self.matchHistoryTableView.reload()
+//                    }
+//                    else {
+//                        print("Request failed cause: \(errorMsg ?? "No error description")")
+//                    }
                 }
+                
             }
             else {
                 print("Request failed cause: \(errorMsg ?? "No error description")")
@@ -49,14 +52,14 @@ class MatchHistoryViewController: UIViewController {
         }
     }
     
-    func getMatchDetails(for gameId: GameId, completion: @escaping (Match) -> Void) {
-        if let localGameDetails = self.matchDetails[gameId] {
+    func getMatchDetails(matchId: LOLMatchId, completion: @escaping (Match?) -> Void) {
+        if let localGameDetails = self.matchDetails[matchId] {
             completion(localGameDetails)
         }
         else {
-            league.lolAPI.getMatch(by: gameId, on: preferredRegion) { (game, errorMsg) in
+            league.lolAPI.getMatch(by: matchId, on: preferredRegion) { (game, errorMsg) in
                 if let game = game {
-                    self.matchDetails[gameId] = game
+                    self.matchDetails[matchId] = game
                     completion(game)
                 }
                 else {
@@ -66,8 +69,8 @@ class MatchHistoryViewController: UIViewController {
         }
     }
     
-    func getChampionImage(championId: ChampionId, completion: @escaping (UIImage?) -> Void) {
-        league.lolAPI.getChampionDetails(by: championId) { (champion, errorMsg) in
+    func getChampionImage(participant: MatchParticipant, completion: @escaping (UIImage?) -> Void) {
+        league.lolAPI.getChampionDetails(by: participant.championId) { (champion, errorMsg) in
             if let champion = champion, let defaultSkin = champion.images?.square {
                 defaultSkin.getImage() { (image, error) in
                     completion(image)
@@ -78,6 +81,7 @@ class MatchHistoryViewController: UIViewController {
             }
         }
     }
+    
     
     func getSummonerSpellImage(summonerSpellId: SummonerSpellId, completion: @escaping (UIImage?) -> Void) {
         league.lolAPI.getSummonerSpell(by: summonerSpellId) { (summonerSpell, errorMsg) in
@@ -97,14 +101,14 @@ class MatchHistoryViewController: UIViewController {
         var spell1Image: UIImage?
         var spell2Image: UIImage?
         var received: Int = 0
-        self.getSummonerSpellImage(summonerSpellId: participant.summonerSpell1) { image in
+        self.getSummonerSpellImage(summonerSpellId: participant.summoner1Id) { image in
             spell1Image = image
             received += 1
             if received == 2 {
                 completion(spell1Image, spell2Image)
             }
         }
-        self.getSummonerSpellImage(summonerSpellId: participant.summonerSpell2) { image in
+        self.getSummonerSpellImage(summonerSpellId: participant.summoner2Id) { image in
             spell2Image = image
             received += 1
             if received == 2 {
@@ -151,42 +155,44 @@ extension MatchHistoryViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let matchAtIndex: MatchReference = self.matches[indexPath.row]
-        return self.setupMatchCell(match: matchAtIndex)
+        let matchAtIndex: LOLMatchId = self.matches[indexPath.row]
+        return self.setupMatchCell(matchId: matchAtIndex)
     }
     
-    func setupMatchCell(match: MatchReference) -> MatchTableViewCell {
+    func setupMatchCell(matchId: LOLMatchId) -> MatchTableViewCell {
         let newCell: MatchTableViewCell = self.matchHistoryTableView.dequeueReusableCell(withIdentifier: "matchCell") as! MatchTableViewCell
         guard let summoner = self.summoner else { return newCell }
-        self.getMatchDetails(for: match.gameId) { matchDetails in
-            
-            if let summonerParticipantId = matchDetails.participants.filter({ participant in
-                return participant.player.summonerId == summoner.id
-            }).first?.participantId {
-                if let summonerParticipant = matchDetails.participantsInfo.filter({ participant in
-                    return participant.participantId == summonerParticipantId
-                }).first {
-                    if let summonerWon = matchDetails.teamsInfo.filter({ team in
-                        return team.teamId == summonerParticipant.teamId
-                    }).first?.win {
+        self.getMatchDetails(matchId: matchId) { matchDetails in
+            if let matchDetails = matchDetails {
+                if let player = matchDetails.info.participants.first(where: { $0.summonerId == summoner.id}) {
+                    if player.win {
                         DispatchQueue.main.async {
-                            newCell.backgroundColor = summonerWon ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+                            newCell.backgroundColor = player.win ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
                         }
                     }
-                    self.getSummonerSpellImages(participant: summonerParticipant) { (spell1Image, spell2Image) in
+
+                    self.getChampionImage(participant: player) { image in
+                        newCell.championSquare.setImage(image)
+                    }
+                    self.getSummonerSpellImages(participant: player) { (spell1Image, spell2Image) in
                         newCell.spell1Square.setImage(spell1Image)
                         newCell.spell2Square.setImage(spell2Image)
                     }
                 }
+
+                newCell.matchDuration.setText("\(matchDetails.info.gameDuration)")
+                
+                let timeString = Datetime(timestamp: matchDetails.info.gameStartTimestamp).toString(format: "dd/MM/yy")
+                newCell.matchDate.setText(timeString)
+                newCell.mapName.setText(Map(Long(matchDetails.info.mapId)).note)
             }
             
-            newCell.matchDuration.setText(matchDetails.gameDuration.toString())
         }
-        self.getChampionImage(championId: match.championId) { image in
-            newCell.championSquare.setImage(image)
-        }
-        newCell.matchDate.setText(match.gameDate.toString(format: "dd/MM/yy"))
-        newCell.mapName.setText(match.queue.place.rawValue)
+        
+        
+        
+        
+                                //queue.place.rawValue)
         return newCell
     }
 }
@@ -195,8 +201,8 @@ extension MatchHistoryViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: false)
-        let matchAtIndex: MatchReference = self.matches[indexPath.row]
-        guard let matchDetails = self.matchDetails[matchAtIndex.gameId] else { return }
+        let matchAtIndex: LOLMatchId = self.matches[indexPath.row]
+        guard let matchDetails = self.matchDetails[matchAtIndex] else { return }
         self.performSegue(withIdentifier: "showParticipants", sender: matchDetails)
     }
 }
